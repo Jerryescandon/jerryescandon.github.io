3e0f1884e9d7d99ed270228f7b1d51770c37568b
Jerbo Home <jerryescandon@gmail.com>
2018-08-02 15:16:27 -0400

    Upload site from desktop

---
+++ js/map.js
@@ -0,0 +1,119 @@
+/* ----------------------------------------------------
+
+    File Name: map.js
+    Template Name: Linda
+    Created By: HTML.Design
+    http://themeforest.net/user/wpdestek
+
+    ITS WORK ONLY CONTACT PAGE
+
+------------------------------------------------------- */
+
+    (function($) {
+        "use strict";
+        var locations=[ ['<div class="infobox"><h3 class="title"></h3><span>OTTAWA</span></div>',
+        45.413995, -75.685661,
+        2]];
+        var map=new google.maps.Map(document.getElementById('map'), {
+            zoom: 14, scrollwheel: false, navigationControl: true, mapTypeControl: false, scaleControl: false, draggable: true, styles: [
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#1d8bd7"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+], center: new google.maps.LatLng(45.413995, -75.685661), mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        
+        );
+        var infowindow=new google.maps.InfoWindow();
+        var marker,
+        i;
+        for (i=0;
+        i < locations.length;
+        i++) {
+            marker=new google.maps.Marker( {
+                position: new google.maps.LatLng(locations[i][1], locations[i][2]), map: map, icon: 'upload/marker.png'
+            }
+            );
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                }
+            }
+            )(marker, i));
+        }
+    })(jQuery);
